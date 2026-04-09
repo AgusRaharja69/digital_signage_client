e395b81bd39e3a21fb84b6873e142ae12c5ea6be
AgusRaharja69 <raharja.wahyu.agus.kadek@gmail.com>
2026-04-09 19:21:00 +0800

    update time

--- db/update_data.py
+++ db/update_data.py
@@ -91,8 +91,8 @@ def update_config(c):
         'school_name':         'SMAN 1 TABANAN',
         'wifi_ssid':           'awlr_wifi',
         'wifi_password':       'Warmadewa2025',
-        'time_off': '19:10',
-        'time_on': '19:13',
+        'time_off': '19:25',
+        'time_on': '19:30',
         'photo_drive_webhook': 'https://script.google.com/macros/s/AKfycbwp5U6C7j9BTBrOjYIXgEiXJ2XJn6N1u9Ex6SUU3ccMQM4FaD9GDye2haZ7Pp4KOboRMg/exec',
     }
 
